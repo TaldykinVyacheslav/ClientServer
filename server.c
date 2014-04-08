9658f8a8481721fe6df51969c2426980d34fd07e
Slava <zegoline@gmail.com>
2014-04-08 22:47:19 +0300

    server body is refreshed

--- server.c
+++ server.c
@@ -1,128 +1,27 @@
-/*
-	C socket server example, handles multiple clients using threads
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <pthread.h>
 
-#include<stdio.h>
-#include<string.h>	//strlen
-#include<stdlib.h>	//strlen
-#include<sys/socket.h>
-#include<arpa/inet.h>	//inet_addr
-#include<unistd.h>	//write
-#include<pthread.h> //for threading , link with lpthread
+#define SERVER_PORT 12345
+#define QUEUE_SIZE 5
 
-//the thread function
-void *connection_handler(void *);
 
-int main(int argc , char *argv[])
+
+
+void send_file(char* filename, int socket)
 {
-	int socket_desc , client_sock , c , *new_sock;
-	struct sockaddr_in server , client;
-	
-	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (socket_desc == -1)
-	{
-		printf("Could not create socket");
-	}
-	puts("Socket created");
-	
-	//Prepare the sockaddr_in structure
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( 8888 );
-	
-	//Bind
-	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-		//print the error message
-		perror("bind failed. Error");
-		return 1;
-	}
-	puts("bind done");
-	
-	//Listen
-	listen(socket_desc , 3);
-	
-	//Accept and incoming connection
-	puts("Waiting for incoming connections...");
-	c = sizeof(struct sockaddr_in);
-	
-	
-	//Accept and incoming connection
-	puts("Waiting for incoming connections...");
-	c = sizeof(struct sockaddr_in);
-	while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-	{
-		puts("Connection accepted");
-		
-		pthread_t sniffer_thread;
-		new_sock = malloc(1);
-		*new_sock = client_sock;
-		
-		if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
-		{
-			perror("could not create thread");
-			return 1;
-		}
-		
-		//Now join the thread , so that we dont terminate before the thread
-		//pthread_join( sniffer_thread , NULL);
-		puts("Handler assigned");
-	}
-	
-	if (client_sock < 0)
-	{
-		perror("accept failed");
-		return 1;
-	}
-	
-	return 0;
+
 }
 
-/*
- * This will handle connection for each client
- * */
-void *connection_handler(void *socket_desc)
+void *thread_func(void *sock)
 {
-	//Get the socket descriptor
-	int sock = *(int*)socket_desc;
-	int read_size;
-	char *message , fileName[100];
-	
-	//Receive a message from client
-	while( (read_size = recv(sock , fileName , 100 , 0)) > 0 )
-	{
-		//Open file and know its size
-		FILE *f = fopen(fileName, "rb");
-		fseek(f, 0, SEEK_END);
-		long fsize = ftell(f);
-		fseek(f, 0, SEEK_SET);
 
-		//Read file content
-		char *message = malloc(fsize + 1);
-		fread(message, fsize, 1, f);
-		fclose(f);
-		
-		//Set buffer as null-terminated string
-		message[fsize] = 0;
+}
 
-		//Send the file content back to client
-		write(sock , message , strlen(message));
-	}
-	
-	if(read_size == 0)
-	{
-		puts("Client disconnected");
-		fflush(stdout);
-	}
-	else if(read_size == -1)
-	{
-		perror("recv failed");
-		return (void*)1;
-	}
-		
-	//Free the socket pointer
-	free(socket_desc);
-	
-	return 0;
+void main (int argc, char *argv[])
+{
+
 }
