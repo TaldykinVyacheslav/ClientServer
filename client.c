e3c1e14fdb726f4f5a51ab03b6431c864ed31baf
Slava <zegoline@gmail.com>
2014-04-08 22:43:04 +0300

    added client connection initialization

--- client.c
+++ client.c
@@ -11,5 +11,26 @@
 
 main (int argc, char *argv[])
 {
-	
+	int len, error;
+	int sockfd;
+	char send_buf[80];
+	struct sockaddr_in addr;
+
+	if (argc != 3) {
+		perror("wrong args");
+		exit(-1);
+	}
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+	{
+		perror("socket");
+		exit(-1);
+	}
+
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family      = AF_INET;
+	addr.sin_addr.s_addr = inet_addr(argv[1]);
+	addr.sin_port        = htons(SERVER_PORT);
 }
